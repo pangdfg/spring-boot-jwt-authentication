b4a10f1a85dc9428a79cd92c0b4b67a4db34a698
poiu <ponpottboomk@gmail.com>
2026-03-22 18:58:02 +0700

    inite

---
+++ spring-auth-jwt/src/main/java/com/example/spring_auth_jwt/SpringAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_auth_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthJwtApplication.class, args);
+	}
+
+}
